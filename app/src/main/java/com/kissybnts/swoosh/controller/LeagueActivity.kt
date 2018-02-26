7f267f8df76854930a3d3a1bdce703ac62bff469
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-26 22:40:28 +0900

    Lecture 55, Instance State

--- app/src/main/java/com/kissybnts/swoosh/controller/LeagueActivity.kt
+++ app/src/main/java/com/kissybnts/swoosh/controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.kissybnts.swoosh.controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.kissybnts.swoosh.R
@@ -13,9 +14,21 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player()
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onMensClicked(view: View) {
